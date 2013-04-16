9b2cdd76df75b5af225b70f00eae162d886a01da
Christian Ledermann <christian.ledermann@gmail.com>
2013-04-16 19:08:38 +0300

    add __geo_interface__

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='pyshp',
+      version='1.1.4',
+      description='Pure Python read/write support for ESRI Shapefile format',
+      long_description=open('README.txt').read(),
+      author='Joel Lawhead',
+      author_email='jlawhead@geospatialpython.com',
+      url='http://code.google.com/p/pyshp',
+      py_modules=['shapefile'],
+      license='MIT',
+      zip_safe=False,
+      keywords='gis geospatial geographic shapefile shapefiles',
+      classifiers=['Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: GIS',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Software Development :: Libraries :: Python Modules'])
